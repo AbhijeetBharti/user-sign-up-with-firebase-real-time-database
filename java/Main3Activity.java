bef1320da8b6aebb3a4f80949f04ae32a5ef2f6e
Abhijeet Bharti <37837646+AbhijeetBharti@users.noreply.github.com>
2019-01-23 03:03:03 +0530

    Add files via upload

---
+++ java/Main3Activity.java
@@ -0,0 +1,17 @@
+package com.example.abhijeet.myapplication;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+private DrawerLayout mDrawerLayout;
+private ActionBarDrawerToggle mToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
